af6cc7ddedaace11aa86155508801dab8b2411e3
ccalobeto <calobeto.leon@gmail.com>
2024-09-17 18:13:36 -0500

    upload file by chunks (lower than 2GiB)

--- scripts/upload2cloudflare.js
+++ scripts/upload2cloudflare.js
@@ -1,5 +1,10 @@
-import { CreateBucketCommand, S3Client, PutObjectCommand } from "@aws-sdk/client-s3";
+import { CreateBucketCommand, S3Client, CreateMultipartUploadCommand, UploadPartCommand, CompleteMultipartUploadCommand } from "@aws-sdk/client-s3";
 import 'dotenv/config'
+import * as fs from "fs";
+
+const bucketName = "s3-demo-bucket"; // new bucket
+const filename = "peru.pmtiles"; // new filename
+const filePath = `./download-pmtiles/${filename}`;
 
 const s3 = new S3Client({ 
   region: "auto",
@@ -27,26 +32,67 @@ export const createBucket = async (bucketName) => {
   }
 }
 
-export const upLoadFile = async (bucketName, filename, fileContent) => {
+export const upLoadFile = async (bucketName, filename, filePath) => {
   console.log(`Uploading file: ${filename} to bucket ${bucketName}`);
-  const command = new PutObjectCommand({ 
-    Bucket: bucketName, 
-    Key: filename, 
-    Body: fileContent
-   });
+  const fileBuffer = fs.readFileSync(filePath);
+  const fileSize = fileBuffer.length;
+  const chunkSize = 500 * 1024 * 1024; // 500 MB chunks
+  const numChunks = Math.ceil(fileSize / chunkSize);
 
-   try {
-     const { ETag } = await s3.send(command);
-     console.log(`File ${filename} uploaded with ETag: ${ETag}`);
-   } catch (error) {
-     console.error(`Error uploading file: ${filename} to bucket ${bucketName}`, error);
-   }
-}
+  const createMultipartUploadParams = {
+    Bucket: bucketName,
+    Key: filename
+  }
+
+  const createMultipartUploadCommand = new CreateMultipartUploadCommand(createMultipartUploadParams);
+  const createMultipartUploadResponse = await s3.send(createMultipartUploadCommand);
+  const uploadId = createMultipartUploadResponse.UploadId;
+
+  const parts = [];
+
+  for (let i = 0; i < numChunks; i++) {
+    const start = i * chunkSize;
+    const end = Math.min(start + chunkSize - 1, fileSize - 1);
+    const chunk = fileBuffer.slice(start, end + 1);
+  
+    const uploadPartParams = {
+      Bucket: bucketName,
+      Key: filename,
+      PartNumber: i + 1,
+      UploadId: uploadId,
+      Body: chunk,
+    };
+  
+    const uploadPartCommand = new UploadPartCommand(uploadPartParams);
+    const uploadPartResponse = await s3.send(uploadPartCommand);
+    parts.push({
+      ETag: uploadPartResponse.ETag,
+      PartNumber: i + 1,
+    });
+  }
+
+  const completeMultipartUploadParams = {
+    Bucket: bucketName,
+    Key: filename,
+    UploadId: uploadId,
+    MultipartUpload: {
+      Parts: parts
+    }
+  }
+  
+  const completeMultipartUploadCommand = new CompleteMultipartUploadCommand(completeMultipartUploadParams);
+
+  try {
+    const { ETag } = await s3.send(completeMultipartUploadCommand);
+    console.log(`File ${filename} uploaded with ETag: ${ETag}`);
+  } catch (error) {
+    console.error(`Error uploading file: ${filename} to bucket ${bucketName}`, error);
+  }
+}  
 
 const main = async () => {
-  const bucketName = "s3-demo-bucket";
   await createBucket(bucketName);
-  await upLoadFile(bucketName, "test2.txt", "Hello World 2");
+  await upLoadFile(bucketName, filename, filePath);
 }
 
 main()
